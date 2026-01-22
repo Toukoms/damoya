b39eaec30f0afa9ca34c090ca6f2da26be7bde3a
toki <toki.rahajanirina@gmail.com>
2026-01-22 18:09:46 +0300

    fix: adjust header layout and button cursor style

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -6,7 +6,7 @@ import { SearchInput } from "../ui";
 export function Header() {
   return (
     <header>
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex gap-8 items-center mb-4">
         <div className="w-32 md:w-48 lg:w-60 aspect-video relative">
           <Image
             src="/logo.png"
@@ -18,7 +18,7 @@ export function Header() {
 
         <div className="flex gap-4 lg:gap-6">
           <SearchInput />
-          <button className="border border-secondary p-2 rounded-sm">
+          <button className="border border-secondary p-2 rounded-sm cursor-pointer">
             <FiShoppingCart size={24} className="text-secondary" />
           </button>
         </div>
